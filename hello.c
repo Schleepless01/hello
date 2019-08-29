70606f594492d4da04cdfa981dfd7b0ec07bd6c1
Pete Tungtweegait <m3110dream.xtremely@gmail.com>
2019-08-29 08:04:58 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,5 @@
 /**
- * Author: Pete Tungtweegait 
+ * Author: Pete Tungtweegait
  * Date: 2019/08/29
  *
  * A simple hello world program in C
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Pete Tungtweegait\n");
+  printf("Computer Engineering\n");
 
   return 0;
 }
